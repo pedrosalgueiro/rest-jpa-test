2fa57ab16d61cff3abcc926d3a1bb9538133984e
Pedro Salgueiro <pds@uevora.pt>
2020-05-13 22:52:11 +0100

    deploy variables tweaks

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@
 
 ssh -o StrictHostKeyChecking=no deploy@deves.xdi.uevora.pt << 'ENDSSH'
   cd ./app
-  export $(cat .env | xargs)
   sudo docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
   sudo docker-compose -f docker-compose.prod.yml up -d
 ENDSSH
--- setup_env.sh
+++ setup_env.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo DB_USER=$DB_USER >> .env
-echo DB_PASSWORD=$DB_PASSWORD >> .env
-echo DB_URL=$DB_URL >> .env
-echo DB_NAME=$DB_NAME >> .env
+echo DB_USER=$DB_USER >> db.env
+echo DB_PASSWORD=$DB_PASSWORD >> db.env
+echo DB_URL=$DB_URL >> db.env
+echo DB_NAME=$DB_NAME >> db.env
