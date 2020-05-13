d14142f54826e630c8f525c9d9ac4dfe099cd284
Pedro Salgueiro <pds@uevora.pt>
2020-05-13 21:54:00 +0100

    missing sudo, again

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 ssh -o StrictHostKeyChecking=no deploy@deves.xdi.uevora.pt << 'ENDSSH'
   cd ./app
   export $(cat .env | xargs)
-  docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
+  sudo docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
   sudo docker-compose up -d
 ENDSSH
