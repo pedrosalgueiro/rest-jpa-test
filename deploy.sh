e9aadb337a10336e38021430c53f0db5b7864e1d
Pedro Salgueiro <pds@uevora.pt>
2020-05-13 21:39:21 +0100

    fixed hostname deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ssh -o StrictHostKeyChecking=no root@$DIGITAL_OCEAN_IP_ADDRESS << 'ENDSSH'
+ssh -o StrictHostKeyChecking=no deploy@deves.xdi.uevora.pt << 'ENDSSH'
   cd ./app
   export $(cat .env | xargs)
   docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
