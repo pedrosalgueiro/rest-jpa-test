15540e5a15969bf149ca98cfd4c3fbb04fb7e4e4
pedrosalgueiro <pds@uevora.pt>
2025-05-22 11:21:17 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 ssh -o StrictHostKeyChecking=no deploy@deves.xdi.uevora.pt << 'ENDSSH'
   cd ./app
   docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
-  docker-compose -f docker-compose.prod.yml up -d
+  docker compose -f docker-compose.prod.yml up -d
 ENDSSH
